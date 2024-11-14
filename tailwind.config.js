d25b87f2450d30881a5c780489dff1d32cd1fd1b
abrorb3k <abrorb3k0219@gmail.com>
2024-11-14 14:55:36 +0500

    fayllar yaratildi

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-green": "#079211",
+        "kulrang": "#808080",
+        "dark-gray": "#313131",
+      },
+    },
+  },
+  plugins: [],
+};
+
